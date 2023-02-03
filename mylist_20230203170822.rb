51ffdf146a47bd3313e8e8f6156e6e383a53ebd4
187jjay187 <187jjay187@gmail.com>
2023-02-03 18:21:37 +0200

    Add mylist.rb my test.rb

--- mylist_20230203170822.rb
+++ mylist_20230203170822.rb
@@ -4,11 +4,10 @@ class MyList
   def initialize(*list)
     @list = list
   end
-  ​
+
   def listeach
     return to_enum(:listeach) unless block_given?
 
-    ​
     counter = 0
     while counter < @list.length
       yield(@list[counter])
